39ea6435870f5e34ba66f826c7e2297b96b0ef94
PedroAnjosFerreira <69936395+PedroAnjosFerreira@users.noreply.github.com>
2022-03-15 10:38:28 -0300

    Add files via upload

---
+++ Revisão/Jaca.js
@@ -0,0 +1,11 @@
+function cultura () {
+    document.location.href="cultura.html"
+}
+
+function historia () {
+   document.location.href="historia.html"
+}
+
+function pontos_turisticos () {
+document.location.href="pontos_turisticos.html"
+}
